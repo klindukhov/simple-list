c58a100675a17973fb5269b8a3ed232a0f785a3b
klindukhov <-@->
2023-08-05 13:59:33 +0200

    change list structure to keyed-by-object-id, add tags UI, add separate state for tags and make them unique

--- simple-list/src/App.tsx
+++ simple-list/src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { v4 as uuidv4 } from "uuid";
 import { getList as getListApi, setList as setListApi } from "./api";
+import TrashCanCursor from "./assets/TrashCanCursor24fat.png";
 
 export interface ListItem {
   id: string;
@@ -12,51 +13,98 @@ export interface ListItem {
 
 export default function App() {
   useEffect(() => {
-    getListApi().then((list) => setList(list));
+    getListApi().then((list) => {
+      setList(list);
+      setTagsList(generateTagsList(list));
+    });
   }, []);
 
-  const [list, setList] = useState<ListItem[]>([]);
-  const [focusedListItem, setFocusedItemList] = useState<ListItem>({
+  const [list, setList] = useState<{ [itemId: string]: ListItem }>({});
+  const [tagsList, setTagsList] = useState<{ [tag: string]: boolean }>({});
+  const [focusedListItem, setFocusedListItem] = useState<ListItem>({
     id: "0",
     summary: "",
     description: "",
     tags: [],
   });
+  const [newTag, setNewTag] = useState("");
+
+  const generateTagsList = (itemList: {
+    [itemId: string]: ListItem;
+  }): { [tag: string]: boolean } => {
+    if (Object.keys(itemList).length === 0) return {};
+
+    const allTags = Object.keys(itemList)
+      .map((id) => itemList[id].tags)
+      .reduce((allTags, tags) => [...allTags, ...tags]);
+
+    const uniqueTags = Array.from(new Set(allTags));
+
+    let tagsHighlightList: { [tag: string]: boolean } = {};
+    uniqueTags.forEach((tag) => {
+      tagsHighlightList[tag] = false;
+    });
+    return tagsHighlightList;
+  };
+
+  const addNewTagToList = (tag: string) => {
+    if (Object.keys(tagsList).includes(tag)) return;
+    let tempTagsList: { [tag: string]: boolean } = {};
+    Object.assign(tempTagsList, tagsList);
+
+    tempTagsList[tag] = false;
+
+    setTagsList(tempTagsList);
+  };
+
+  const removeTagFromList = (tag: string) => {
+    let tempTagsList: { [tag: string]: boolean } = {};
+    Object.assign(tempTagsList, tagsList);
+
+    delete tempTagsList[tag];
+
+    setTagsList(tempTagsList);
+  };
 
   const setListItemSummary = (id: string, summary: string) => {
-    let tempList = [...list];
-    let listItemIndex = list.findIndex((el) => el.id === id);
-    if (!listItemIndex && listItemIndex !== 0) return;
-    tempList[listItemIndex].summary = summary;
+    let tempList: { [itemId: string]: ListItem } = {};
+    Object.assign(tempList, list);
+
+    tempList[id].summary = summary;
+
     setList(tempList);
     setListApi(tempList);
   };
 
   const createNewListItem = () => {
-    if (list.length > 0 && list[list.length - 1].summary === "") {
-      let tempList = [...list];
-      tempList.pop();
-      setList(tempList);
-      setListApi(tempList);
-      return;
+    let tempList: { [itemId: string]: ListItem } = {};
+    Object.assign(tempList, list);
+
+    if (
+      Object.keys(list).length > 0 &&
+      list[Object.keys(list)[Object.keys(list).length - 1]].summary === ""
+    ) {
+      delete tempList[Object.keys(list)[Object.keys(list).length - 1]];
+    } else {
+      const newId = uuidv4();
+      tempList[newId] = {
+        id: newId,
+        summary: "",
+        description: "",
+        tags: ["Created"],
+      };
     }
-    let tempList = [...list];
-    tempList.push({
-      id: uuidv4(),
-      summary: "",
-      description: "",
-      tags: ["newElement"],
-    });
+
     setList(tempList);
     setListApi(tempList);
   };
 
   const removeElement = (id: string) => {
-    let tempList = [...list];
-    tempList.splice(
-      tempList.findIndex((e) => e.id === id),
-      1
-    );
+    let tempList: { [itemId: string]: ListItem } = {};
+    Object.assign(tempList, list);
+
+    delete tempList[id];
+
     setList(tempList);
     setListApi(tempList);
   };
@@ -65,7 +113,7 @@ export default function App() {
     window.addEventListener("keypress", (e) => {
       if (e.key === "Enter") createNewListItem();
     });
-    setFocusedItemList(listItem);
+    setFocusedListItem(listItem);
   };
 
   //temporary as would not make sense after ading description, tags
@@ -73,37 +121,86 @@ export default function App() {
     if (summary === "") removeElement(id);
   };
 
+  const addTag = (id: string, tag: string) => {
+    let tempList: { [itemId: string]: ListItem } = {};
+    Object.assign(tempList, list);
+
+    if (tempList[id].tags.includes(tag)) {
+      return;
+    } else {
+      tempList[id].tags.push(tag);
+    }
+
+    setList(tempList);
+    setListApi(tempList);
+    addNewTagToList(tag);
+  };
+
+  const removeTag = (id: string, tag: string) => {
+    let tempList: { [itemId: string]: ListItem } = {};
+    Object.assign(tempList, list);
+
+    if (tempList[id].tags.includes(tag)) {
+      tempList[id].tags.splice(tempList[id].tags.indexOf(tag), 1);
+    }
+
+    setList(tempList);
+    setListApi(tempList);
+    if (!Object.keys(generateTagsList(tempList)).includes(tag))
+      removeTagFromList(tag);
+  };
+
   return (
     <MainPage>
       <TagSidePanel>
-        {list.length !== 0 &&
-          list
-            .map((li) => li.tags)
-            .reduce((allTags, tags) => [...allTags, ...tags])
-            .map((tag) => (
-              <TagElement>
-                <InputField style={{ textAlign: "center" }} value={tag} />
-              </TagElement>
-            ))}
+        {Object.keys(tagsList).map((tag) => (
+          <TagElement>{tag}</TagElement>
+        ))}
       </TagSidePanel>
       <ListPanel onClick={createNewListItem}>
-        {list.length !== 0 &&
-          list.map((le) => (
-            <ListElement key={le.id} onClick={(e) => e.stopPropagation()}>
+        {Object.keys(list).length !== 0 &&
+          Object.keys(list).map((id) => (
+            <ListElement key={id} onClick={(e) => e.stopPropagation()}>
               <ListElementInputField
-                value={le.summary}
+                value={list[id].summary}
                 onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                  setListItemSummary(le.id, e.target.value);
+                  setListItemSummary(id, e.target.value);
                 }}
-                onFocus={() => handleItemFocus(le)}
-                onBlur={() => deleteEmptyOnBlur(le.id, le.summary)}
+                onFocus={() => handleItemFocus(list[id])}
+                onBlur={() => deleteEmptyOnBlur(id, list[id].summary)}
               />
             </ListElement>
           ))}
       </ListPanel>
       <ItemDetailsPanel>
         <DetailsSummary>{focusedListItem.summary}</DetailsSummary>
-        <DetailsTags></DetailsTags>
+        <DetailsTags>
+          {focusedListItem.tags.map((t) => (
+            <TagChip
+              style={{ width: t.length + "ch" }}
+              onClick={() => removeTag(focusedListItem.id, t)}
+            >
+              {t}
+            </TagChip>
+          ))}
+          {focusedListItem.summary.length > 0 && (
+            <AddTagChip
+              style={{
+                width: (newTag.length + 1 < 2 ? 2 : newTag.length + 1) + "ch",
+                cursor: "pointer",
+              }}
+              value={newTag}
+              onChange={(e) => setNewTag(e.target.value)}
+              placeholder='+'
+              onBlur={() => {
+                if (newTag !== "+" && newTag !== "") {
+                  addTag(focusedListItem.id, newTag);
+                }
+                setNewTag("");
+              }}
+            />
+          )}
+        </DetailsTags>
       </ItemDetailsPanel>
     </MainPage>
   );
@@ -145,6 +242,7 @@ const TagElement = styled.div`
       margin-top: 1rem;
     }
   }
+  cursor: pointer;
 `;
 
 const ListPanel = styled.div`
@@ -182,14 +280,6 @@ const ListElementInputField = styled.input`
   width: 50rem;
 `;
 
-const InputField = styled.input`
-  background-color: transparent;
-  border-color: transparent;
-  color: inherit;
-  outline: none;
-  min-width: 1rem;
-`;
-
 const ItemDetailsPanel = styled.div`
   height: 100vh;
   overflow: scroll;
@@ -201,8 +291,37 @@ const ItemDetailsPanel = styled.div`
 
 const DetailsSummary = styled.div`
   color: Black;
-  font-size: 20pt;
-  padding-top: 1rem;
+  font-size: 22pt;
+  padding: 1rem;
+  min-height: 2rem;
 `;
 
-const DetailsTags = styled.div``;
+const DetailsTags = styled.div`
+  padding: 1rem;
+  padding-top: 0rem;
+`;
+
+const AddTagChip = styled.input`
+  background-color: lightgrey;
+  color: #000000be;
+  border-radius: 0.2rem;
+  border-width: 0px;
+  text-align: center;
+  margin-right: 0.5rem;
+  margin-bottom: 0.5rem;
+  height: 1.2rem;
+  display: inline-block;
+  vertical-align: baseline;
+`;
+
+const TagChip = styled.div`
+  display: inline-block;
+  height: 1.2rem;
+  background-color: lightgrey;
+  color: #000000be;
+  border-radius: 0.2rem;
+  text-align: center;
+  margin-right: 0.5rem;
+  margin-bottom: 0.5rem;
+  cursor: url(${TrashCanCursor}), crosshair;
+`;
--- simple-list/src/api.ts
+++ simple-list/src/api.ts
@@ -1,7 +1,7 @@
 import { ListItem } from "./App";
 
-export const getList = async (): Promise<ListItem[]> =>
-  await JSON.parse(localStorage.getItem("simpleList") ?? "[]");
+export const getList = async (): Promise<{ [itemId: string]: ListItem }> =>
+  await JSON.parse(localStorage.getItem("simpleList") ?? "{}");
 
-export const setList = (list: ListItem[]) =>
+export const setList = (list: { [itemId: string]: ListItem }) =>
   localStorage.setItem("simpleList", JSON.stringify(list));
