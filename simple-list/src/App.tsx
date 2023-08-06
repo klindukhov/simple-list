af01df1aeda876515a7e9de6973c6cc9fb964a5a
klindukhov <-@->
2023-08-06 13:30:28 +0200

    add inclusive/exclusive filtering by tags

--- simple-list/src/App.tsx
+++ simple-list/src/App.tsx
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import { v4 as uuidv4 } from "uuid";
 import { getList as getListApi, setList as setListApi } from "./api";
 import TrashCanCursor from "./assets/TrashCanCursor24fat.png";
+import InclusiveDiagram from "./assets/InclusiveDiagramFat.png";
+import ExclusiveDiagram from "./assets/ExclusiveDiagramFat.png";
 
 export interface ListItem {
   id: string;
@@ -21,13 +23,15 @@ export default function App() {
 
   const [list, setList] = useState<{ [itemId: string]: ListItem }>({});
   const [tagsList, setTagsList] = useState<{ [tag: string]: boolean }>({});
-  const [focusedListItem, setFocusedListItem] = useState<ListItem>({
-    id: "0",
-    summary: "",
-    description: "",
-    tags: [],
-  });
+  const [isFilteringInclusive, setIsFilteringInclusive] = useState(true);
+  const [focusedListItemId, setFocusedListItemId] = useState<string>("0");
   const [newTag, setNewTag] = useState("");
+
+  useEffect(() => {
+    if (Object.keys(list).length > 0 && list[focusedListItemId] === undefined) {
+      setFocusedListItemId(Object.keys(list)[0]);
+    }
+  }, [list]);
 
   const generateTagsList = (itemList: {
     [itemId: string]: ListItem;
@@ -66,46 +70,51 @@ export default function App() {
     setTagsList(tempTagsList);
   };
 
-  const setListItemSummary = (id: string, summary: string) => {
-    let tempList: { [itemId: string]: ListItem } = {};
-    Object.assign(tempList, list);
+  const setListItemSummary = async (id: string, summary: string) => {
+    let tempList: { [itemId: string]: ListItem } = await getListApi();
 
     tempList[id].summary = summary;
 
-    setList(tempList);
+    setList(await getFilteredList(tagsList, tempList, isFilteringInclusive));
     setListApi(tempList);
   };
 
-  const createNewListItem = () => {
-    let tempList: { [itemId: string]: ListItem } = {};
-    Object.assign(tempList, list);
+  const createNewListItem = async () => {
+    let tempList: { [itemId: string]: ListItem } = await getListApi();
 
+    const lastElementId =
+      Object.keys(tempList)[Object.keys(tempList).length - 1];
     if (
-      Object.keys(list).length > 0 &&
-      list[Object.keys(list)[Object.keys(list).length - 1]].summary === ""
+      Object.keys(tempList).length > 0 &&
+      tempList[lastElementId].summary === ""
     ) {
-      delete tempList[Object.keys(list)[Object.keys(list).length - 1]];
+      delete tempList[lastElementId];
     } else {
       const newId = uuidv4();
+      let newTags: string[] = [];
+      Object.keys(tagsList).forEach((tag) => {
+        if (tagsList[tag] && tag !== "Created") {
+          newTags.push(tag);
+        }
+      });
       tempList[newId] = {
         id: newId,
         summary: "",
         description: "",
-        tags: ["Created"],
+        tags: ["Created", ...newTags],
       };
     }
 
-    setList(tempList);
+    setList(await getFilteredList(tagsList, tempList, isFilteringInclusive));
     setListApi(tempList);
   };
 
-  const removeElement = (id: string) => {
-    let tempList: { [itemId: string]: ListItem } = {};
-    Object.assign(tempList, list);
+  const removeElement = async (id: string) => {
+    let tempList: { [itemId: string]: ListItem } = await getListApi();
 
     delete tempList[id];
 
-    setList(tempList);
+    setList(await getFilteredList(tagsList, tempList, isFilteringInclusive));
     setListApi(tempList);
   };
 
@@ -113,17 +122,11 @@ export default function App() {
     window.addEventListener("keypress", (e) => {
       if (e.key === "Enter") createNewListItem();
     });
-    setFocusedListItem(listItem);
+    setFocusedListItemId(listItem.id);
   };
 
-  //temporary as would not make sense after ading description, tags
-  const deleteEmptyOnBlur = (id: string, summary: string) => {
-    if (summary === "") removeElement(id);
-  };
-
-  const addTag = (id: string, tag: string) => {
-    let tempList: { [itemId: string]: ListItem } = {};
-    Object.assign(tempList, list);
+  const addTag = async (id: string, tag: string) => {
+    let tempList: { [itemId: string]: ListItem } = await getListApi();
 
     if (tempList[id].tags.includes(tag)) {
       return;
@@ -131,30 +134,125 @@ export default function App() {
       tempList[id].tags.push(tag);
     }
 
-    setList(tempList);
+    setList(await getFilteredList(tagsList, tempList, isFilteringInclusive));
     setListApi(tempList);
     addNewTagToList(tag);
   };
 
-  const removeTag = (id: string, tag: string) => {
-    let tempList: { [itemId: string]: ListItem } = {};
-    Object.assign(tempList, list);
+  const removeTag = async (id: string, tag: string) => {
+    let tempList: { [itemId: string]: ListItem } = await getListApi();
 
     if (tempList[id].tags.includes(tag)) {
       tempList[id].tags.splice(tempList[id].tags.indexOf(tag), 1);
     }
 
-    setList(tempList);
+    setList(await getFilteredList(tagsList, tempList, isFilteringInclusive));
     setListApi(tempList);
     if (!Object.keys(generateTagsList(tempList)).includes(tag))
       removeTagFromList(tag);
   };
 
+  const highlightTag = async (tag: string) => {
+    let tempTagsList: { [tag: string]: boolean } = {};
+    Object.assign(tempTagsList, tagsList);
+
+    tempTagsList[tag] = !tempTagsList[tag];
+
+    setTagsList(tempTagsList);
+    setList(
+      await getFilteredList(
+        tempTagsList,
+        await getListApi(),
+        isFilteringInclusive
+      )
+    );
+  };
+
+  const getFilteredList = async (
+    tempTagsList: {
+      [tag: string]: boolean;
+    },
+    fullList: { [itemId: string]: ListItem },
+    isFilteringInclusiveLocal: boolean
+  ): Promise<{ [itemId: string]: ListItem }> => {
+    if (!Object.values(tempTagsList).some((tagHighlight) => tagHighlight)) {
+      return fullList;
+    }
+
+    let filteredList: { [itemId: string]: ListItem } = {};
+    const activeTags = Object.keys(tempTagsList).filter(
+      (tag) => tempTagsList[tag]
+    );
+
+    if (isFilteringInclusiveLocal) {
+      activeTags.forEach((tag) => {
+        Object.keys(fullList).forEach((listItemId) => {
+          if (
+            fullList[listItemId].tags.includes(tag) &&
+            !Object.keys(filteredList).includes(listItemId)
+          ) {
+            filteredList[listItemId] = fullList[listItemId];
+          }
+        });
+      });
+    } else {
+      Object.assign(filteredList, fullList);
+      activeTags.forEach((tag) => {
+        Object.keys(fullList).forEach((listItemId) => {
+          if (!fullList[listItemId].tags.includes(tag)) {
+            delete filteredList[listItemId];
+          }
+        });
+      });
+    }
+
+    return filteredList;
+  };
+
   return (
     <MainPage>
       <TagSidePanel>
+        <FilteringType>
+          <FilteringTypeElement
+            style={{
+              backgroundColor: isFilteringInclusive
+                ? "transparent"
+                : "lightgrey",
+            }}
+            onClick={async () => {
+              setIsFilteringInclusive(false);
+              setList(
+                await getFilteredList(tagsList, await getListApi(), false)
+              );
+            }}
+          >
+            <img src={ExclusiveDiagram} style={{ height: "2rem" }} />
+          </FilteringTypeElement>
+          <FilteringTypeElement
+            style={{
+              backgroundColor: isFilteringInclusive
+                ? "lightgrey"
+                : "transparent",
+            }}
+            onClick={async () => {
+              setIsFilteringInclusive(true);
+              setList(
+                await getFilteredList(tagsList, await getListApi(), true)
+              );
+            }}
+          >
+            <img src={InclusiveDiagram} style={{ height: "2rem" }} />
+          </FilteringTypeElement>
+        </FilteringType>
         {Object.keys(tagsList).map((tag) => (
-          <TagElement>{tag}</TagElement>
+          <TagElement
+            style={{
+              backgroundColor: tagsList[tag] ? "lightgrey" : "transparent",
+            }}
+            onClick={() => highlightTag(tag)}
+          >
+            {tag}
+          </TagElement>
         ))}
       </TagSidePanel>
       <ListPanel onClick={createNewListItem}>
@@ -167,41 +265,42 @@ export default function App() {
                   setListItemSummary(id, e.target.value);
                 }}
                 onFocus={() => handleItemFocus(list[id])}
-                onBlur={() => deleteEmptyOnBlur(id, list[id].summary)}
               />
             </ListElement>
           ))}
       </ListPanel>
-      <ItemDetailsPanel>
-        <DetailsSummary>{focusedListItem.summary}</DetailsSummary>
-        <DetailsTags>
-          {focusedListItem.tags.map((t) => (
-            <TagChip
-              style={{ width: t.length + "ch" }}
-              onClick={() => removeTag(focusedListItem.id, t)}
-            >
-              {t}
-            </TagChip>
-          ))}
-          {focusedListItem.summary.length > 0 && (
-            <AddTagChip
-              style={{
-                width: (newTag.length + 1 < 2 ? 2 : newTag.length + 1) + "ch",
-                cursor: "pointer",
-              }}
-              value={newTag}
-              onChange={(e) => setNewTag(e.target.value)}
-              placeholder='+'
-              onBlur={() => {
-                if (newTag !== "+" && newTag !== "") {
-                  addTag(focusedListItem.id, newTag);
-                }
-                setNewTag("");
-              }}
-            />
-          )}
-        </DetailsTags>
-      </ItemDetailsPanel>
+      {focusedListItemId !== "0" && list[focusedListItemId] && (
+        <ItemDetailsPanel>
+          <DetailsSummary>{list[focusedListItemId].summary}</DetailsSummary>
+          <DetailsTags>
+            {list[focusedListItemId].tags.map((t) => (
+              <TagChip
+                style={{ width: t.length + "ch" }}
+                onClick={() => removeTag(focusedListItemId, t)}
+              >
+                {t}
+              </TagChip>
+            ))}
+            {list[focusedListItemId].summary.length > 0 && (
+              <AddTagChip
+                style={{
+                  width: (newTag.length + 1 < 2 ? 2 : newTag.length + 1) + "ch",
+                  cursor: "pointer",
+                }}
+                value={newTag}
+                onChange={(e) => setNewTag(e.target.value)}
+                placeholder='+'
+                onBlur={() => {
+                  if (newTag !== "+" && newTag !== "") {
+                    addTag(focusedListItemId, newTag);
+                  }
+                  setNewTag("");
+                }}
+              />
+            )}
+          </DetailsTags>
+        </ItemDetailsPanel>
+      )}
     </MainPage>
   );
 }
@@ -223,7 +322,10 @@ const TagSidePanel = styled.div`
 `;
 
 const TagElement = styled.div`
-  background-color: lightgrey;
+  border-width: 2px;
+  border-style: solid;
+  border-color: lightgrey;
+  color: black;
   height: 3rem;
   display: grid;
   align-items: center;
@@ -324,4 +426,38 @@ const TagChip = styled.div`
   margin-right: 0.5rem;
   margin-bottom: 0.5rem;
   cursor: url(${TrashCanCursor}), crosshair;
+  padding: 0.1rem;
+`;
+
+const FilteringType = styled.div`
+  margin: 0rem;
+  padding: 0rem;
+  border-width: 2px;
+  border-style: solid;
+  border-color: lightgrey;
+  margin-top: 1rem;
+  margin-bottom: 0.1rem;
+  display: grid;
+  grid-template-columns: 50% 50%;
+  width: 20rem;
+  column-gap: 0rem;
+  border-radius: 0.5rem 0.5rem 0rem 0rem;
+`;
+
+const FilteringTypeElement = styled.div`
+  background-color: lightgrey;
+  border: none;
+  height: 3rem;
+  display: grid;
+  align-items: center;
+  justify-items: center;
+  width: 10rem;
+  margin: 0rem;
+  &:first-child {
+    border-radius: 0.5rem 0rem 0rem 0rem;
+  }
+  &:last-child {
+    border-radius: 0rem 0.5rem 0rem 0rem;
+  }
+  cursor: pointer;
 `;
