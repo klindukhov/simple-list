863e1f0f9ef8fd7b04c220f95a4b93bc96cd4a96
klindukhov <61867600+klindukhov@users.noreply.github.com>
2024-05-31 20:25:15 +0200

    implement persistence using file system (hardcoded path)

--- src/api.ts
+++ src/api.ts
@@ -1,11 +1,24 @@
 import { Filter, ListItem } from "./App";
 import * as localForage from "localforage";
+import { Filesystem, Directory, Encoding } from "@capacitor/filesystem";
 
-export const getList = async (): Promise<{ [itemId: string]: ListItem }> =>
-  (await localForage.getItem("simpleList")) ?? {};
+export const getList = async (): Promise<{ [itemId: string]: ListItem }> => {
+  const contents = await Filesystem.readFile({
+    path: "Documents/LocalSimpleList/SimpleList.json",
+    directory: Directory.ExternalStorage,
+    encoding: Encoding.UTF8,
+  });
+  return JSON.parse(contents.data.toString() ?? "");
+};
 
-export const setList = (list: { [itemId: string]: ListItem }) =>
-  localForage.setItem("simpleList", list);
+export const setList = (list: { [itemId: string]: ListItem }) => {
+  Filesystem.writeFile({
+    path: "Documents/LocalSimpleList/SimpleList.json",
+    data: JSON.stringify(list),
+    directory: Directory.ExternalStorage,
+    encoding: Encoding.UTF8,
+  });
+};
 
 export const getSavedFilters = async (): Promise<{
   [filterSetId: string]: { [filterId: string]: Filter };
